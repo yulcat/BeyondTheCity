062f45a439ed3eee93af15aef4b9d3e804fca2f3
Lee, Kyuho <modulo641@gmail.com>
2015-11-02 18:31:46 +0900

    Test scene Currently using unity's built-in physics. Needs to make our own physics.

---
+++ Assets/Scripts/Characters/CharacterController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BeyondTheCity
+{
+	public class CharacterController : MonoBehaviour 
+	{
+		enum State
+		{
+			onGround, onBack, onAir
+		}
+		State characterState = State.onGround;
+		
+		void OnCollisionStay(Collision2D coll)
+		{
+			characterState = State.onGround;
+		}
+		void OnCollisionExit(Collision2D coll)
+		{
+			characterState = State.onAir;
+		}
+		Rigidbody2D body;
+		
+		public float baseSpeed = 0.01f;
+		public float jumpPower = 0.1f;
+		float WalkSpeed()
+		{
+			return baseSpeed;
+		}
+		
+		void Awake()
+		{
+			body = GetComponent<Rigidbody2D>();
+		}
+		void Update()
+		{
+			transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * baseSpeed);
+			
+			if (Input.GetKeyDown("space") && characterState == State.onGround)
+			{
+				Debug.Log("Jump");
+				StartCoroutine(Jump());
+			}
+		}
+		
+		IEnumerator Jump()
+		{
+			float counter = 0.1f;
+			while(counter > 0)
+			{
+				body.AddForce(Vector2.up * jumpPower);
+				counter -= Time.deltaTime;
+				yield return null;
+			}
+			yield break;
+		}
+	}	
+}
---
+++ Assets/Scripts/Characters/MetaController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BeyondTheCity
+{
+	public abstract class Controller
+	{
+		public GameObject character;
+		public CharacterController charControl
+		{
+			get
+			{
+				return character.GetComponent<CharacterController>();
+			}
+		}
+		
+	}
+	
+	public class KeyboardControll : Controller
+	{
+		
+	}
+	
+	public class MetaController : MonoBehaviour 
+	{
+		public CharacterController[] players;
+		int playerCount = 0;
+		
+		void Awake()
+		{
+			
+		}
+		
+		void Update()
+		{
+			if (Input.GetKeyDown("tab"))
+			{
+				players[playerCount].enabled = false;
+				playerCount++;
+				if (playerCount == 3)
+					playerCount = 0;
+				players[playerCount].enabled = true;
+			}
+		}
+		
+		void TestInitializer()
+		{
+			
+		}
+	}	
+}
