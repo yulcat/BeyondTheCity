7d5bf05570b44698787aa19d301c4e5b103f17be
Lee, Kyuho <modulo641@gmail.com>
2015-11-22 19:00:19 +0900

    캐릭터와 관련된 클래스 및 스크립트 재설정, 태그 구분

---
+++ Assets/Scripts/Characters/AnimalBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalBehavior : MonoBehaviour 
+{
+	public float jumpPower;
+	public float moveSpeed;
+	public void Jump()
+	{
+		
+	}
+	public void Walk()
+	{
+		
+	}
+}
