85c74bd63f51082122b8b85280c31f149487f3c8
Lee, Kyuho <modulo641@gmail.com>
2015-11-27 17:45:46 +0900

    Wall

---
+++ Assets/Scripts/Characters/ContactDetector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContactDetector : MonoBehaviour 
+{
+	public AnimalBehavior owner;
+	public string targetTag;
+	
+	void OnTriggerStay2D(Collider2D coll)
+	{
+		if (coll.transform.tag == targetTag)
+			owner.isGrounded = true;
+	}
+	void OnTriggerExit2D(Collider2D coll)
+	{
+		if (coll.transform.tag == targetTag)
+			owner.isGrounded = false;
+	}
+}
+
