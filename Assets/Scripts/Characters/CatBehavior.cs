7b3571b97183ff679a5b341df08edc7cda5356ea
yulcat <alszmtm@naver.com>
2015-12-03 10:10:16 +0900

    사람 머리위에 이모티콘 마크 크기 정상화

--- Assets/Scripts/Characters/CatBehavior.cs
+++ Assets/Scripts/Characters/CatBehavior.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class CatBehavior : AnimalBehavior
 {
 	public bool isWalled = false;
+	bool wallOnRight;
 	
 	void OnCollisionEnter2D(Collision2D coll)
 	{
 		if (coll.gameObject.tag == "Wall")
+		{
 			isWalled = true;
+			wallOnRight = coll.transform.position.x > transform.position.x;
+		}
 	}
 	
 	void OnCollisionExit2D(Collision2D coll)
@@ -19,10 +23,17 @@ public class CatBehavior : AnimalBehavior
 	
 	public override void Jump()
 	{
-		if (isGrounded || isWalled)
+		if (isGrounded)
 		{
 			//  StartCoroutine(JumpUp());
 			body.velocity = new Vector2(body.velocity.x, jumpPower);
+		}
+		else if(isWalled)
+		{
+			int dirc;
+			if(wallOnRight) dirc = -1;
+			else dirc = 1;
+			body.velocity = new Vector2(jumpPower*dirc, jumpPower);
 		}
 	}
 	
