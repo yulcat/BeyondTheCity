602f1de4f1de47cb68b78610964aae8b4fad29e4
Lee, Kyuho <modulo641@gmail.com>
2015-12-16 16:51:27 +0900

    크레딧, 설정 추가

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour 
+{
+	public GameObject mainMenu;
+	void Update()
+	{
+		if (Input.GetButtonDown("Bark1"))
+		{
+			mainMenu.SetActive(true);
+			gameObject.SetActive(false);
+		}
+	}
+}
