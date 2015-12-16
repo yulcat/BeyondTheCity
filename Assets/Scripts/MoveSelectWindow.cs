b2c37dd63e189ca74acf2499ff892cff9d9c92ed
yulcat <alszmtm@naver.com>
2015-12-16 18:15:39 +0900

    날린 스테이지 선택창 재작업

--- Assets/Scripts/MoveSelectWindow.cs
+++ Assets/Scripts/MoveSelectWindow.cs
@@ -4,18 +4,37 @@ using UnityEngine.UI;
 
 public class MoveSelectWindow : MonoBehaviour {
 	public GameObject firstSelected;
+	public GameObject mainMenu;
+	public GameObject stageIcons;
+	public Text text;
+	public string[] stageNames;
 	public void MoveWindowAt (int target) {
 		Debug.Log(target);
+		text.text = stageNames[target];
 		iTween.ValueTo(gameObject,iTween.Hash("onupdate","PositionUpdate","onupdatetarget",gameObject,
-			"from",gameObject.GetComponent<RectTransform>().localPosition.x,
+			"from",stageIcons.GetComponent<RectTransform>().localPosition.x,
 			"to",target*-300f,
 			"time",0.3f,
 			"easetype",iTween.EaseType.easeOutCubic));
 	}
 	void PositionUpdate(float position) {
-		gameObject.GetComponent<RectTransform>().localPosition = Vector3.right * position;
+		stageIcons.GetComponent<RectTransform>().localPosition = Vector3.right * position;
 	}
 	void OnEnable () {
 		EventSystem.current.SetSelectedGameObject(firstSelected);
+	}
+	public void SelectStage(int stage){
+		Application.LoadLevel(stage + 1);
+	}
+	public void BackToMain(){
+		mainMenu.SetActive(true);
+		gameObject.SetActive(false);
+		EventSystem.current.SetSelectedGameObject(EventSystem.current.firstSelectedGameObject);
+	}
+	
+	void Update(){
+		if(Input.GetButtonDown("Bark1")){
+			BackToMain();
+		}
 	}
 }
