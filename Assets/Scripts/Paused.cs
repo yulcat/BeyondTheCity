c30f2bdfe9e567a476b7c0c3ba3f21c4b3cb53a5
yulcat <alszmtm@naver.com>
2015-12-17 02:00:32 +0900

    일시정지 창 추가됨

---
+++ Assets/Scripts/Paused.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Paused : MonoBehaviour {
+	GameObject prevSelectedObject;
+	public GameObject resumeGame;
+	
+	void OnEnable(){
+		prevSelectedObject = EventSystem.current.currentSelectedGameObject;
+		EventSystem.current.SetSelectedGameObject(resumeGame);
+		resumeGame.GetComponent<Button>().Select();
+	}
+	void Update(){
+		if(Input.GetButtonDown("Bark1")){
+			ResumeGame();
+		}
+	}
+	void ResumeGame(){
+		gameObject.SetActive(false);
+		EventSystem.current.SetSelectedGameObject(prevSelectedObject);
+	}
+	public void Select(bool quitting){
+		if(!quitting){
+			ResumeGame();
+			return;
+		}
+		Application.Quit();
+	}
+}
