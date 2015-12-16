b6c2a8fe33cd02f151d73ca2dfde3151c7b6e4fb
Lee, Kyuho <modulo641@gmail.com>
2015-12-17 00:45:31 +0900

    인풋 테스트(백업용)

---
+++ Assets/Scripts/PadController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using GamepadInput;
+
+public class PadController : MonoBehaviour 
+{
+	public int padNumber;
+	public AnimalBehavior targetCharacter;
+	GamePad.Index TargetPad
+	{
+		get
+		{
+			if (padNumber == 1)
+				return GamePad.Index.One;
+			else if (padNumber == 2)
+				return GamePad.Index.Two;
+			else if (padNumber == 3)
+				return GamePad.Index.Three;
+			else
+				return GamePad.Index.Four;	
+		}
+	}
+	void Update()
+	{
+		MoveCommand(GamePad.GetAxis(GamePad.Axis.LeftStick, TargetPad).x);
+		if (GamePad.GetButtonDown(GamePad.Button.A, TargetPad))
+			targetCharacter.Bark();
+		if (GamePad.GetButtonDown(GamePad.Button.A, TargetPad))
+			targetCharacter.Jump();
+		if (GamePad.GetAxis(GamePad.Axis.LeftStick, TargetPad).y > 1)
+			targetCharacter.Interact();
+		if (GamePad.GetAxis(GamePad.Axis.LeftStick, TargetPad).y < -1)
+			targetCharacter.DownFloor();
+	}
+	
+	void MoveCommand(float moveInput)
+	{
+		targetCharacter.Move(moveInput);
+	}
+	void JumpCommand()
+	{
+		targetCharacter.Jump();
+	}
+	void BarkCommand()
+	{
+		targetCharacter.Bark();
+	}
+	void InteractCommand()
+	{
+		targetCharacter.Bark();
+	}
+	void DownCommand()
+	{
+		targetCharacter.DownFloor();
+	}
+}
