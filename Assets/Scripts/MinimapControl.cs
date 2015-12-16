2f2800783d5b763ab2c65be433709331d39e7643
Lee, Kyuho <modulo641@gmail.com>
2015-12-16 20:40:43 +0900

    미니맵 카메라 도입. Tab 누르고 있는 동안 켜짐. 각 씬에 적용 중.

---
+++ Assets/Scripts/MinimapControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapControl : MonoBehaviour 
+{
+	public GameObject minimap;
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.Tab))
+		{
+			minimap.SetActive(true);
+		}
+		else if(Input.GetKeyUp(KeyCode.Tab))
+		{
+			minimap.SetActive(false);
+		}
+	}
+}
