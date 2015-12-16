d5d2efc845274b8fde8d46005e14b1dbd0be1761
Lee, Kyuho <modulo641@gmail.com>
2015-12-16 17:09:49 +0900

    새 게임 시작시 페이드아웃

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MainMenu : MonoBehaviour 
 {
+	public GameObject animals;
+	public Image mask;
+	public AnimalBehavior[] friends;
 	public void StartNewLevel()
 	{
+		StartCoroutine(NewLevel());
+	}
+	IEnumerator NewLevel()
+	{
+		float timer = 0f;
+		float timing = 1f;
+		float animalSpeed = 7f;
+		while (timer < timing)
+		{
+			float percentage = timer/timing;
+			mask.color = new Color(0, 0, 0, percentage);
+			animals.transform.Translate(animalSpeed * Time.deltaTime * Vector2.right);
+			timer += Time.deltaTime;
+			yield return new WaitForFixedUpdate();
+		}
 		Application.LoadLevel(1);
+		yield break;
 	}
 }
