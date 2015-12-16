90d48f286917c035bf09641dc7e2608420a32a96
yulcat <alszmtm@naver.com>
2015-12-16 17:37:48 +0900

    스테이지 선택창 작업중

---
+++ Assets/Scripts/MoveSelectWindow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MoveSelectWindow : MonoBehaviour {
+	public GameObject firstSelected;
+	public void MoveWindowAt (int target) {
+		Debug.Log(target);
+		iTween.ValueTo(gameObject,iTween.Hash("onupdate","PositionUpdate","onupdatetarget",gameObject,
+			"from",gameObject.GetComponent<RectTransform>().localPosition.x,
+			"to",target*-300f,
+			"time",0.3f,
+			"easetype",iTween.EaseType.easeOutCubic));
+	}
+	void PositionUpdate(float position) {
+		gameObject.GetComponent<RectTransform>().localPosition = Vector3.right * position;
+	}
+	void OnEnable () {
+		EventSystem.current.SetSelectedGameObject(firstSelected);
+	}
+}
