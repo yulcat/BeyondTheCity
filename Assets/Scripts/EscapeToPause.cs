c933105d13f0aa9dadf80ab869d5ef24cfa95d88
yulcat <alszmtm@naver.com>
2015-12-17 02:29:59 +0900

    esc 버튼으로 일시정지 창이 뜸

---
+++ Assets/Scripts/EscapeToPause.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeToPause : MonoBehaviour {
+	public GameObject pauseMenu;
+	// Update is called once per frame
+	void Update () {
+		if(pauseMenu.activeSelf) return;
+		if(Input.GetKey(KeyCode.Escape)){
+			pauseMenu.SetActive(true);
+		}
+	}
+}
