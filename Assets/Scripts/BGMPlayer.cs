b9b0ba6119fd3abe1cd1481ed8a13bfbde9aca77
yulcat <alszmtm@naver.com>
2015-12-17 21:39:10 +0900

    브금 재생기 추가됨

---
+++ Assets/Scripts/BGMPlayer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class BGMPlayer : MonoBehaviour {
+	static BGMPlayer _instance;
+	public AudioClip[] clips;
+	int prevScene = -1;
+	AudioSource source;
+
+	// Use this for initialization
+	void Awake () {
+		prevScene = Application.loadedLevel;
+		if(_instance!=null)
+			Destroy(gameObject);
+		else
+			_instance = this;
+		source = GetComponent<AudioSource>();
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void OnLevelWasLoaded(int level) {
+		if(prevScene==level)
+			return;
+		if(level==0){
+			source.clip = clips[0];
+		}else{
+			source.clip = clips[1];
+		}
+		source.Play();
+		prevScene = level;
+	}
+	
+	void Update(){
+		transform.position = Camera.main.transform.position;
+	}
+}
